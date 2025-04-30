bf6fa9ae3b501beab9ecf35f78e9d4fa60439779
Blake Byrnes <blakebyrnes@gmail.com>
2025-04-30 15:43:42 -0400

    chore: handle env for node

--- env.ts
+++ env.ts
@@ -1,13 +1,15 @@
-declare module 'process' {
-  interface ProcessEnv {
-    PORT: string;
-    KEYPAIR_PATH: string;
-    KEYPAIR_PASSPHRASE?: string;
-    LOCAL_RPC_URL: string;
-    ARCHIVE_NODE_URL: string;
-    BIDDING_RULES_PATH: string;
-    DATADIR: string;
-    SESSION_KEYS_MNEMONIC: string;
-    OLDEST_ROTATION_TO_SYNC?: string;
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string;
+      KEYPAIR_PATH: string;
+      KEYPAIR_PASSPHRASE?: string;
+      LOCAL_RPC_URL: string;
+      ARCHIVE_NODE_URL: string;
+      BIDDING_RULES_PATH: string;
+      DATADIR: string;
+      SESSION_KEYS_MNEMONIC: string;
+      OLDEST_ROTATION_TO_SYNC?: string;
+    }
   }
 }
